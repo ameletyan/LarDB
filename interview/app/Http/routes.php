6264866d834dd8e202b3a550954bbd0a793f3d69
Artur Meletyan <artm95@gmail.com>
2015-05-23 00:08:15 -0600

    finally comfortable with PHP and Laravel

--- interview/app/Http/routes.php
+++ interview/app/Http/routes.php
@@ -7,16 +7,15 @@
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| and give it the controller to call when that URI is requested.
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', 'WelcomeController@index');
 
-Route::get('users', function()
-{
-    return View::make('users');
-});
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
--- interview/database/migrations/2014_10_12_000000_create_users_table.php
+++ interview/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,15 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('email')->unique();
+			$table->string('password', 60);
+			$table->rememberToken();
+			$table->timestamps();
+		});
 	}
 
 	/**
@@ -22,7 +30,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('users');
 	}
 
 }
