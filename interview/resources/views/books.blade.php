de64c57a49c090283fb2f66a4652406f45025691
Artur Meletyan <artm95@gmail.com>
2015-05-23 13:31:25 -0600

    added a simple format, not final

--- interview/resources/views/books.blade.php
+++ interview/resources/views/books.blade.php
@@ -1,7 +1,8 @@
 @extends('layout')
-
-@section('sidebar')
+@section('content')
 <?php
+	// the code below is a little off, make sure to check over it
+	// the implementation of the above code will not have the same effect as the code below
 
 	$username = "artm95";
 	$password = "1022";
@@ -15,7 +16,7 @@
 	$selected = mysqli_select_db($link,"interview") 
 	  or die("Could not select interview");
 
-	//execute the SQL query and return records
+	//execute the SQL query and return authors
 	$result = mysqli_query($link,"SELECT name FROM authors");
 	//fetch the data from the database
 	$authors = array();
@@ -24,69 +25,9 @@
 	}
 
 	echo Form::open(array('books' => '/author.php'));
-		echo Form::select('author', $authors);
-		echo Form::submit('Submit');
 		$author = Input::get('author')+1;
-		echo $author;
 	echo Form::close();
-
-	//close the connection
-	mysqli_close($link);
-
-?>
-
-@section('content')
-<?php
-	// the code below is a little off, make sure to check over it
-	// the implementation of the above code will not have the same effect as the code below
-	echo $author;
-
-	$username = "artm95";
-	$password = "1022";
-	$hostname = "localhost";
-
-	//connection to the database
-	$link = mysqli_connect($hostname, $username, $password) 
-	  or die("Unable to connect to MySQL");
-
-	//select a database to work with
-	$selected = mysqli_select_db($link,"interview") 
-	  or die("Could not select interview");
-
-	// //execute the SQL query and return records
-	// $result = mysqli_query($link,"SELECT author_id, title FROM books");
-	// //fetch the data from the database
-	// $books = array();
-	// while ($row = mysqli_fetch_array($result)) {
-	//    array_push($books, $row{'author_id'});
-	//    array_push($books, $row{'title'});
-	// }
-
-	switch ($author) {
-		case 1:
-			echo "Victor Hugo";
-			break;
-
-		case 2:
-			echo "Jules Verne";
-			break;
-
-		case 3:
-			echo "Alexandre Dumas";
-			break;
-
-		case 4:
-			echo "Leo Tolstoy";
-			break;
-
-		case 5:
-			echo "H.G. Wells";
-			break;
-		
-		default:
-			break;
-	}
-
+	
 	//execute the SQL query and return book titles
 	$result = mysqli_query($link,"SELECT title FROM books WHERE author_id = $author");
 	//fetch the data from the database
@@ -109,12 +50,6 @@
 		echo $books[$i];
 		echo $texts[$i];
 	}
-	// foreach ($books as $book) {
-	// 	echo $book;
-	// }
-	// foreach ($texts as $text) {
-	// 	echo $text;
-	// }
 
 	//close the connection
 	mysqli_close($link);
--- interview/resources/views/layout.blade.php
+++ interview/resources/views/layout.blade.php
@@ -1,7 +1,45 @@
 <html>
-	<head>BOOKS</head>
+	<h1>BOOKS</h1>
     <body>
-    	@yield('sidebar')
-        @yield('content')
+        <div class="container">
+        	<br><?php
+
+				$username = "artm95";
+				$password = "1022";
+				$hostname = "localhost";
+
+				//connection to the database
+				$link = mysqli_connect($hostname, $username, $password) 
+				  or die("Unable to connect to MySQL");
+
+				//select a database to work with
+				$selected = mysqli_select_db($link,"interview") 
+				  or die("Could not select interview");
+
+				//execute the SQL query and return records
+				$result = mysqli_query($link,"SELECT name FROM authors");
+				//fetch the data from the database
+				$authors = array();
+				while ($row = mysqli_fetch_array($result)) {
+				   array_push($authors, $row{'name'});
+				}
+
+				echo Form::open(array('books' => '/author.php'));
+					echo Form::select('author', $authors);
+					echo Form::submit('Submit');
+					$author = Input::get('author')+1;
+				echo Form::close();
+
+				//close the connection
+				mysqli_close($link);
+
+			?><br>
+        </div>
+
+        <h2>{{ $authors[$author-1] }}</h2>
+        <!-- @yield('content') -->
+        {{ $books[1] }}
+
     </body>
 </html>
+
